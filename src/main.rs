573778de386a005be4cda213496d7f6f5821e310
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-13 22:02:37 +0000

    Extract login from email

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+use lazy_static::lazy_static;
+use regex::Regex;
+
+fn main() {
+    fn extract_login(email: &str) -> String {
+        // regex pattern to extract login from email
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^(?P<login>[^@]+)@").unwrap();
+        }
+        // extract login from email
+        let login = RE.captures(email).unwrap().name("login").unwrap().as_str();
+        // return login
+        login.to_string()
+    }
+
+    let login = extract_login("ron_burgundy.121.310_2132-hellyaaa!@hollywoods_ass.com");
+    println!("{}", login);
+}
